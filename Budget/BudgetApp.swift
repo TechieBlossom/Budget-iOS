fbf22c54b8dee062cb18075c4ab3acdb522d4387
Prateek Sharma <prateeks@getbaraka.com>
2025-09-05 11:06:48 +0400

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,32 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Prateek Sharma on 05/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BudgetApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BudgetTests/BudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTests.swift
+//  BudgetTests
+//
+//  Created by Prateek Sharma on 05/09/2025.
+//
+
+import Testing
+@testable import Budget
+
+struct BudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
